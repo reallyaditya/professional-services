f398749b29e9c9b8f0ea9039a19d083288391b90
Holt Skinner <holtskinner@google.com>
2019-08-02 09:44:45 -0700

    Updated Dependencies Installation

--- examples/cloudml-document-understanding/dependencies.sh
+++ examples/cloudml-document-understanding/dependencies.sh
@@ -1,2 +1,9 @@
-brew install imagemagick
-brew install ghostscript
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove
+
+sudo apt-get install python3.7 python3-pip -y
+sudo apt-get install python3-numpy python3-matplotlib python3-scipy python3-pandas python3-simpy -y
+
+sudo apt-get install git -y
+sudo apt-get install ghostscript imagemagick -y
+
+
